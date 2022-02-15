2269d7492ddc2a002511479b35fba8d496c8116d
Nadia <nadiawangberg98@gmail.com>
2022-02-15 14:14:38 -0600

    Allow running with or without seg_frame

--- launch_and_plot.sh
+++ launch_and_plot.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
 # $1 - scene_name (office / office_w_people)
 # $2 - duration (2m)
 # $3 - rviz (true / false)
 
-roslaunch robust_slam kimera_uhumans.launch scene:=$1 record_for_evo:=true d:=$2 rviz:=$3
+
+read -p "use semantic segmentation frame? (true/false) : " SEMANTIC
+
+if [[ "$SEMANTIC" == "true" ]]
+then
+  #Use seg frame
+  roslaunch robust_slam kimera_uhumans.launch scene:=$1 record_for_evo:=true d:=$2 rviz:=$3
+else
+  #Use original - no segmentation frame
+  roslaunch robust_slam kimera_uhumans.launch scene:=$1 record_for_evo:=true d:=$2 rviz:=$3 seg_frame_topic:="none"
+fi
+
+
+#roslaunch robust_slam kimera_uhumans.launch scene:=$1 record_for_evo:=true d:=$2 rviz:=$3 # With seg_frame
+
+#roslaunch robust_slam kimera_uhumans.launch scene:=$1 record_for_evo:=true d:=$2 rviz:=$3 seg_frame_topic:="-" #Without seg_frame
 
 echo "start wait for 2 seconds"
 sleep 2
